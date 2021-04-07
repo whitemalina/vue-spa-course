bbb6e2782d86502712eb848b658f603e315f65e7
WhiteMalina <49318023+whitemalina@users.noreply.github.com>
2021-04-07 19:25:46 +0500

    Major update
    Посты юзера
    Просмотр постов
    Комментирование постов

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,124 +1,198 @@
 const host = 'http://37.46.133.40:5000/api';
 
 const app = new Vue({
-  el: '#app',
-  data: {
-    page: 'index',
-    toasts: [],
-    posts: [],
-    registerForm: {
-      name: null,
-      login: null,
-      password: null
+    el: '#app',
+    data: {
+        page: 'index',
+        toasts: [],
+        posts: [],
+        userPosts: [],
+        showPost: [],
+        commentText: '',
+        registerForm: {
+            name: null,
+            login: null,
+            password: null
+        },
+        loginForm: {
+            login: null,
+            password: null
+        },
+        user: null,
+        postForm: {
+            name: null,
+            text: null,
+            image: null
+        }
     },
-    loginForm: {
-      login: null,
-      password: null
+    mounted() {
+        this.loadLocalData();
+        this.getAllPosts();
+        this.getUserPosts();
     },
-    user: null,
-    postForm: {
-      name: null,
-      text: null,
-      image: null
+    methods: {
+        openPage(pageName) {
+            this.page = pageName;
+        },
+        showToast(text, classes, duration = 2000) {
+            const toast = {text, classes, id: Math.random()};
+
+            this.toasts.push(toast);
+
+            setTimeout(() => {
+                this.toasts.splice(this.toasts.indexOf(toast), 1);
+            }, duration);
+        },
+        async fetchAllPosts() {
+            const response = await fetch(host + '/post');
+            return await response.json();
+        },
+        async getAllPosts() {
+            this.posts = await this.fetchAllPosts();
+        },
+
+
+        getUserPosts() {
+            fetch(host + '/my-post', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${this.user.token}`
+                },
+            })
+                .then(res => {
+                    if (res.status === 200)
+                        this.showToast('Вы вошли', ['alert-success']);
+                    else
+                        this.showToast('Что-то пошло не так! ', ['alert-danger']);
+
+                    return res.json();
+                })
+                .then(data => {
+                    this.userPosts = data;
+                })
+        },
+
+        getPostId(id) {
+            fetch(host + '/post/' + id, {
+                method: 'GET',
+                headers: {},
+            })
+                .then(res => {
+
+                    return res.json();
+                })
+                .then(data => {
+                    this.showPost = data;
+                })
+            this.openPage('post');
+        },
+
+        getPostInPostId(id) {
+            fetch(host + '/post/' + id, {
+                method: 'GET',
+                headers: {},
+            })
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    this.showPost = data;
+                })
+        },
+
+        createComment(id) {
+            let formData = new FormData();
+            formData.append('text', this.commentText);
+            fetch(host + '/post/' + id + '/comment', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${this.user.token}`
+                },
+                body: formData
+            })
+                .then(res => {
+                    if (res.status === 201) {
+                        this.showToast('Коммментарий успешно добавлен', ['alert-success']);
+                        this.getPostInPostId(id)
+                        this.commentText = ''
+                    } else
+                        this.showToast('Что-то пошло не так', ['alert-danger']);
+                })
+
+        },
+
+        register() {
+            fetch(host + '/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.registerForm)
+            })
+                .then(res => {
+                    if (res.status === 201) {
+                        this.showToast('Вы успешно зарегистрировались', ['alert-success']);
+                        this.openPage('login');
+                    } else
+                        this.showToast('Что-то пошло не так', ['alert-danger']);
+                })
+        },
+        login() {
+            fetch(host + '/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.loginForm)
+            })
+                .then(res => {
+                    if (res.status === 200)
+                        this.showToast('Вы вошли', ['alert-success']);
+                    else
+                        this.showToast('Не верный логин или пароль', ['alert-danger']);
+
+                    return res.json();
+                })
+                .then(data => {
+                    this.user = data;
+                    this.saveLocalData();
+                    this.openPage('profile');
+                })
+        },
+        logout() {
+            this.user = null;
+            localStorage.removeItem('user');
+        },
+        saveLocalData() {
+            localStorage.setItem('user', JSON.stringify(this.user));
+        },
+        loadLocalData() {
+            this.user = JSON.parse(localStorage.getItem('user'));
+        },
+        fileSelected() {
+            if (event.target.files.length)
+                this.postForm.image = event.target.files[0];
+        },
+        createPost() {
+            let formData = new FormData();
+            formData.append('name', this.postForm.name);
+            formData.append('text', this.postForm.text);
+            formData.append('image', this.postForm.image);
+
+            fetch(host + '/post', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${this.user.token}`
+                },
+                body: formData
+            })
+                .then(res => {
+                    if (res.status === 201) {
+                        this.showToast('Поздравляем! Пост успешно опубликован!', ['alert-success']);
+                        this.getAllPosts();
+                        this.openPage('profile');
+                    }
+                })
+        }
     }
-  },
-  mounted() {
-    this.loadLocalData();
-    this.getAllPosts();
-  },
-  methods: {
-    openPage(pageName) {
-      this.page = pageName;
-    },
-    showToast(text, classes, duration = 2000) {
-      const toast = {text, classes, id: Math.random()};
-
-      this.toasts.push(toast);
-
-      setTimeout(() => {
-        this.toasts.splice(this.toasts.indexOf(toast), 1);
-      }, duration);
-    },
-    async fetchAllPosts() {
-      const response = await fetch(host + '/post');
-      return await response.json();
-    },
-    async getAllPosts() {
-      this.posts = await this.fetchAllPosts();
-    },
-    register() {
-      fetch(host + '/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(this.registerForm)
-      })
-          .then(res => {
-            if (res.status === 201) {
-              this.showToast('Вы успешно зарегистрировались', ['alert-success']);
-              this.openPage('login');
-            }
-            else
-              this.showToast('Что-то пошло не так', ['alert-danger']);
-          })
-    },
-    login() {
-      fetch(host + '/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(this.loginForm)
-      })
-          .then(res => {
-            if (res.status === 200)
-              this.showToast('Вы вошли', ['alert-success']);
-            else
-              this.showToast('Не верный логин или пароль', ['alert-danger']);
-
-            return res.json();
-          })
-          .then(data => {
-            this.user = data;
-            this.saveLocalData();
-            this.openPage('profile');
-          })
-    },
-    logout() {
-      this.user = null;
-      localStorage.removeItem('user');
-    },
-    saveLocalData() {
-      localStorage.setItem('user', JSON.stringify(this.user));
-    },
-    loadLocalData() {
-      this.user = JSON.parse(localStorage.getItem('user'));
-    },
-    fileSelected() {
-      if(event.target.files.length)
-        this.postForm.image = event.target.files[0];
-    },
-    createPost() {
-      let formData = new FormData();
-      formData.append('name', this.postForm.name);
-      formData.append('text', this.postForm.text);
-      formData.append('image', this.postForm.image);
-
-      fetch(host + '/post', {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${this.user.token}`
-        },
-        body: formData
-      })
-          .then(res => {
-            if(res.status === 201) {
-              this.showToast('Поздравляем! Пост успешно опубликован!', ['alert-success']);
-              this.getAllPosts();
-              this.openPage('profile');
-            }
-          })
-    }
-  }
 });
